863b82b582e2f138a2574c72d009d1c01d4d62b0
Mark Ryall <mark@ryall.name>
2024-02-25 12:34:29 +1000

    create and update more venues and artist details

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,33 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
 
 EventStatus.create!(name: "Confirmed") unless EventStatus.where(name: "Confirmed").exists?
 EventStatus.create!(name: "Cancelled") unless EventStatus.where(name: "Cancelled").exists?
 
-if Rails.env.development?
-  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') unless AdminUser.where(email: "admin@example.com").exists?
+mogwai = Artist.find_or_create_by(name: "Mogwai")
+mogwai.update!(
+  location: "Glasgow",
+  country: "Scotland",
+  genres: %w["Post-Rock Electronic Film-Score Television-Music Ambient],
+)
+jerm = Artist.find_or_create_by(name: "Jerm")
+jerm.update!(
+  location: "Brisbane",
+  country: "Australia",
+  genres: %w[Electronic Darkwave],
+)
+
+tivoli = Venue.find_or_create_by(name: "The Tivoli")
+tivoli.update!(
+  address: "52 Costin St, Fortitude Valley, Brisbane",
+  latitude: "-27.452117",
+  longitude: "153.028929",
+  location_url: "https://maps.app.goo.gl/tWw3Waee4ic48EAd8",
+)
+
+if Rails.env.development? && !AdminUser.where(email: "admin@example.com").exists?
+  AdminUser.create!(
+    email: "admin@example.com",
+    password: "password",
+    password_confirmation: "password",
+  )
 end
