9902bd02cceaea65bfd6aab41b95139145e141ed
Mark Ryall <mark@ryall.name>
2024-02-25 22:43:48 +1000

    allow time zone to be edited

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,5 +1,10 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params(
+    :email,
+    :password,
+    :password_confirmation,
+    :time_zone,
+  )
 
   index do
     selectable_column
@@ -19,6 +24,7 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs do
       f.input :email
+      f.input :time_zone
       f.input :password
       f.input :password_confirmation
     end
