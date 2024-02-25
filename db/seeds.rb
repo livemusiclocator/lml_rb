074eea4eec270bc815b7aea5171078536bf30925
Mark Ryall <mark@ryall.name>
2024-02-25 12:47:49 +1000

    add one of everything to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-EventStatus.create!(name: "Confirmed") unless EventStatus.where(name: "Confirmed").exists?
-EventStatus.create!(name: "Cancelled") unless EventStatus.where(name: "Cancelled").exists?
+confirmed = EventStatus.find_or_create_by!(name: "Confirmed")
+EventStatus.find_or_create_by!(name: "Cancelled")
 
 mogwai = Artist.find_or_create_by(name: "Mogwai")
 mogwai.update!(
@@ -22,6 +22,27 @@ tivoli.update!(
   latitude: "-27.452117",
   longitude: "153.028929",
   location_url: "https://maps.app.goo.gl/tWw3Waee4ic48EAd8",
+)
+
+mogwai_at_tiv = Event.find_or_create_by(name: "Mogwai at The Tivoli 2024")
+mogwai_at_tiv.update!(
+  venue: tivoli,
+  headline_artist: mogwai,
+  event_status: confirmed,
+  date: "2024-02-21",
+  start_time: "2024-02-21 19:30+10",
+  finish_time: "2024-02-21 22:30+10",
+)
+
+mogwai_at_tiv_jerm = EventSet.find_or_create_by(event: mogwai_at_tiv, artist: jerm)
+mogwai_at_tiv_jerm.update!(
+  start_time: "2024-02-21 20:00+10",
+  finish_time: "2024-02-21 21:00+10",
+)
+mogwai_at_tiv_mogwai = EventSet.find_or_create_by(event: mogwai_at_tiv, artist: mogwai)
+mogwai_at_tiv_mogwai.update!(
+  start_time: "2024-02-21 21:30+10",
+  finish_time: "2024-02-21 22:30+10",
 )
 
 if Rails.env.development? && !AdminUser.where(email: "admin@example.com").exists?
