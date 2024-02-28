3fc2d64c63a8bd3aea36a567c8db11e075249596
Mark Ryall <mark@ryall.name>
2024-02-28 23:39:14 +1000

    switch to jbuilder template

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,43 +1,7 @@
+# frozen_string_literal: true
+
 class GigsController < ApplicationController
   def query
-    content = []
-
-    Lml::Gig.all.each do |gig|
-      venue = gig.venue
-      headline_act = gig.headline_act
-      current = {
-        id: gig.id,
-        name: gig.name,
-        start_time: gig.start_time,
-        finish_time: gig.finish_time,
-        venue: {
-          id: venue.id,
-          name: venue.name,
-          address: venue.address,
-          latitude: venue.latitude,
-          longitude: venue.longitude,
-        },
-        headline_act: {
-          id: headline_act.id,
-          name: headline_act.name,
-          genres: headline_act.genres,
-        },
-        sets: [],
-      }
-      gig.sets.each do |set|
-        act = set.act
-        current[:sets] << {
-          act: {
-            id: act.id,
-            name: act.name,
-            genres: act.genres,
-          },
-          start_time: set.start_time,
-        }
-      end
-      content << current
-    end
-
-    render json: content
+    @gigs = Lml::Gig.all
   end
 end
---
+++ app/views/gigs/query.json.jbuilder
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+json.array! @gigs do |gig|
+  json.id gig.id
+  json.name gig.name
+  json.start_time gig.start_time
+  json.finish_time gig.finish_time
+
+  venue = gig.venue
+  json.venue do
+    json.id venue.id
+    json.name venue.name
+    json.address venue.address
+    json.latitude venue.latitude
+    json.longitude venue.longitude
+  end
+
+  headline_act = gig.headline_act
+  json.headline_act do
+    json.id headline_act.id
+    json.name headline_act.name
+    json.genres headline_act.genres
+  end
+
+  json.sets gig.sets do |set|
+    json.start_time set.start_time
+
+    act = set.act
+    json.act do
+      json.id act.id
+      json.name act.name
+      json.genres act.genres
+    end
+  end
+end
