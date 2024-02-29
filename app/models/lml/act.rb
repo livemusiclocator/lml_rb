269325f12395ca774ea7366697a2496dda775342
Mark Ryall <mark@ryall.name>
2024-02-29 19:46:33 +1000

    handle new record with nil genres

--- app/models/lml/act.rb
+++ app/models/lml/act.rb
@@ -1,15 +1,15 @@
 module Lml
   class Act < ApplicationRecord
-    def self.ransackable_attributes(auth_object = nil)
+    def self.ransackable_attributes(_auth_object = nil)
       %w[name country location]
     end
 
-    def self.ransackable_associations(auth_object = nil)
+    def self.ransackable_associations(_auth_object = nil)
       []
     end
 
     def genre_list
-      genres.join(", ")
+      (genres || []).join(", ")
     end
 
     def genre_list=(value)
