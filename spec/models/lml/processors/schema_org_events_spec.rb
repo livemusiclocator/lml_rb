2e8d122f64f9388237a47e9c06d4df7737c3203e
Mark Ryall <mark@ryall.name>
2024-03-23 12:10:17 +1000

    allow venue to be passed to upload

--- spec/models/lml/processors/schema_org_events_spec.rb
+++ spec/models/lml/processors/schema_org_events_spec.rb
@@ -2,9 +2,37 @@ require "rails_helper"
 
 describe Lml::Processors::SchemaOrgEvents do
   describe "process!" do
-    it "creates gigs and venues" do
-      upload = double("upload", content: File.read("spec/files/schema_org_events.json"))
-      Lml::Processors::SchemaOrgEvents.new(upload).process!
+    context "when venue is not specified" do
+      it "creates gigs and venue" do
+        upload = double(
+          "upload",
+          content: File.read("spec/files/tivoli_schema_org_events.json"),
+          venue: nil,
+        )
+
+        Lml::Processors::SchemaOrgEvents.new(upload).process!
+
+        expect(Lml::Venue.count).to eq(1)
+        expect(Lml::Venue.first.name).to eq("The Tivoli")
+        expect(Lml::Gig.count).to eq(16)
+      end
+    end
+
+    context "when venue is specified" do
+      it "creates gigs" do
+        venue = Lml::Venue.create!(name: "The Venue")
+        upload = double(
+          "upload",
+          content: File.read("spec/files/tivoli_schema_org_events.json"),
+          venue: venue,
+        )
+
+        Lml::Processors::SchemaOrgEvents.new(upload).process!
+
+        expect(Lml::Venue.count).to eq(1)
+        expect(Lml::Venue.first.name).to eq("The Venue")
+        expect(Lml::Gig.count).to eq(16)
+      end
     end
   end
 end
