673492c34701e6de6dd09e26566a0f1ea769b633
Mark Ryall <mark@ryall.name>
2024-04-14 09:53:18 +1000

    use Australia/Melbourne in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 if Rails.env.development?
   user = AdminUser.find_or_create_by(email: "admin@example.com")
   user.update!(
-    time_zone: ENV.fetch("TIMEZONE", "Melbourne"),
+    time_zone: ENV.fetch("TIMEZONE", "Australia/Melbourne"),
     password: "password",
   )
 end
