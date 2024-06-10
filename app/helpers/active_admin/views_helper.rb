a0647f56c5fde4899397e1be0c3f1b38dc1c866a
Mark Ryall <mark@ryall.name>
2024-06-10 12:24:32 +1000

    time+date format in admin

--- app/helpers/active_admin/views_helper.rb
+++ app/helpers/active_admin/views_helper.rb
@@ -2,12 +2,12 @@ module ActiveAdmin::ViewsHelper
   def admin_time(time)
     return "-" if time.nil?
 
-    time.iso8601.sub("T", " ")
+    time.strftime("%a %d %b %y %R %z")
   end
 
   def admin_date(date)
     return "-" if date.nil?
 
-    date.iso8601
+    date.strftime("%a %d %b %y")
   end
 end
