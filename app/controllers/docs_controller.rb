9072b06eb0bdc99f91d5fc0628f6eb2f04c35ff6
Jen Smith <jen_does_not_monitor@gardenizr.com>
2025-06-25 08:37:25 +1000

    Api docs now use their own layout - still uses the main application css for now but without the web front end css included

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class DocsController < ApplicationController
+  layout 'layouts/docs'
   def index
     doc_md = File.read(Rails.root.join("APIDOC.md"))
     @content = Kramdown::Document.new(doc_md, input: "GFM").to_html
