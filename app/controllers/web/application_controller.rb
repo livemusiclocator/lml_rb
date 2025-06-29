61d4a401d4263a5a502b09300d583549717b1212
Jen Smith <jen_does_not_monitor@gardenizr.com>
2025-06-30 08:14:17 +1000

    A basic and blank 404 page

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,3 +1,11 @@
+
 class Web::ApplicationController < ApplicationController
   layout 'web/layouts/application'
+  rescue_from ActiveRecord::RecordNotFound, with: :render_custom_not_found
+
+  private
+
+  def render_custom_not_found
+    render 'web/shared/404', status: :not_found, layout: false
+  end
 end
